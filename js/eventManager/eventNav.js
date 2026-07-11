f6c7cf993b9d0807cfe208fb586342955d21a5c8
bodhi <ch.brice30@gmail.com>
2026-07-11 16:35:15 +0200

    navbar #8

--- js/eventManager/eventNav.js
+++ js/eventManager/eventNav.js
@@ -3,18 +3,12 @@ function eventNav() {
 
     document.querySelector('.burger-menu').addEventListener('click',
         () => {
-            let burgerMenuList;
-            let dokanLogo;
 
             document.querySelector('.burger-menu-list').classList.toggle('enabled');
             document.querySelector('.burger-menu').classList.toggle('disabled');
             document.querySelector('.citation-landing-page').classList.toggle('disabled');
             document.querySelector('button').classList.toggle('disabled');
             document.querySelector('.banner').classList.toggle('banner-vanish');
-
-            burgerMenuList = document.querySelector('.burger-menu-list');
-            dokanLogo = document.querySelector('.dokan-logo');
-            burgerMenuList.prepend(dokanLogo);
         }
     )
 }
