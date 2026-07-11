b9e9bf25bbf7de431dc41495d871ad7f29fa4196
bodhi <ch.brice30@gmail.com>
2026-07-11 22:24:55 +0200

    navbar#11

--- js/eventNav.js
+++ js/eventNav.js
@@ -1,9 +1,19 @@
 function eventNav() {
     document.querySelector('.burger-menu').addEventListener('click',
         () => {
+            document.querySelector('.burger-menu').style.display = "none";
+            document.querySelector('.burger-menu-close').style.display = "flex";
             document.querySelector('nav').classList.add("mobile-nav");
             document.querySelector('nav ul').classList.add("mobile-nav-list");
         }
-    )
+    );
+
+    document.querySelector(".burger-menu-close").addEventListener('click', () => {
+        document.querySelector('.burger-menu').style.display = "flex";
+        document.querySelector('.burger-menu-close').style.display = "none";
+        document.querySelector('nav').classList.remove("mobile-nav");
+        document.querySelector('nav ul').classList.remove("mobile-nav-list");
+    })
+
 }
 export default eventNav;
