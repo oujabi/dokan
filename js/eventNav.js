aaae76b2b20ca22345d76c8719c54d20ff8ab57a
bodhi <ch.brice30@gmail.com>
2026-07-11 21:19:44 +0200

    navbar#10

--- js/eventNav.js
+++ js/eventNav.js
@@ -1,14 +1,8 @@
 function eventNav() {
-    if (document.querySelector('.burger-menu-list') === null)  return;
-
     document.querySelector('.burger-menu').addEventListener('click',
         () => {
-
-            document.querySelector('.burger-menu-list').classList.toggle('enabled');
-            document.querySelector('.burger-menu').classList.toggle('disabled');
-            document.querySelector('.citation-landing-page').classList.toggle('disabled');
-            document.querySelector('button').classList.toggle('disabled');
-            document.querySelector('.banner').classList.toggle('banner-vanish');
+            document.querySelector('nav').classList.add("mobile-nav");
+            document.querySelector('nav ul').classList.add("mobile-nav-list");
         }
     )
 }
