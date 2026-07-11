b072d25bc29eb79cde832ba7911f7497c7e7f85f
bodhi <ch.brice30@gmail.com>
2026-07-11 16:59:02 +0200

    pagesfactory#1

--- data/nav.js
+++ data/nav.js
@@ -1,27 +1,27 @@
 const navData = {
   "link#1": {
     "name": "Dokan",
-    "href": "./pages/dokan-intro.html"
+    "href": "./pagesFactory/dokan-intro.html"
   },
 
   "link#2": {
     "name": "Stage de la saison",
-    "href": "./pages/stage-saison.html"
+    "href": "./pagesFactory/stage-saison.html"
   },
 
   "link#3": {
     "name": "Médias",
-    "href": "./pages/medias.html"
+    "href": "./pagesFactory/medias.html"
   },
 
   "link#4": {
     "name": "Contacts",
-    "href": "./pages/inscription.html"
+    "href": "./pagesFactory/inscription.html"
   },
 
   "link#5": {
     "name": "Info pratique",
-    "href": "./pages/info-pratique.html"
+    "href": "./pagesFactory/info-pratique.html"
   }
 };
 
---
+++ js/routing/routing.js
@@ -0,0 +1,6 @@
+function goToPage() {
+    document.querySelector('li a').addEventListener('click', () => {
+        let href = document.querySelector('li a').href;
+
+    });
+}
