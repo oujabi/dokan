4cbf94a33651c8b15a753dc85a427c978789ac08
bodhi <ch.brice30@gmail.com>
2026-07-12 02:47:17 +0200

    navbar#16

--- js/eventNav.js
+++ js/eventNav.js
@@ -5,23 +5,21 @@ function eventNav() {
             document.querySelector('.burger-menu-close').style.display = "flex";
             document.querySelector('nav').classList.add("mobile-nav");
             document.querySelector('nav ul').classList.add("mobile-nav-list");
+            document.querySelector('body').style.overflow = "hidden";
         }
     );
 
-    document.querySelector(".burger-menu-close").addEventListener('click', () => {
+    document.querySelector(".burger-menu-close").addEventListener('click', closeNav)
+
+    document.querySelector("main").addEventListener('click', closeNav)
+
+    function closeNav() {
         document.querySelector('.burger-menu').style.display = "flex";
         document.querySelector('.burger-menu-close').style.display = "none";
         document.querySelector('nav').classList.remove("mobile-nav");
         document.querySelector('nav ul').classList.remove("mobile-nav-list");
-    })
-
-    document.querySelector("main").addEventListener('click', () => {
-        document.querySelector('.burger-menu').style.display = "flex";
-        document.querySelector('.burger-menu-close').style.display = "none";
-        document.querySelector('nav').classList.remove("mobile-nav");
-        document.querySelector('nav ul').classList.remove("mobile-nav-list");
-    })
-
+        document.querySelector('body').style.overflow = "visible";
+    }
 
 }
 export default eventNav;
