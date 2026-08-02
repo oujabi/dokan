d030610cac8b2d2fa28178c730e27d48f30250c3
bodhi <ch.brice30@gmail.com>
2026-08-02 19:06:02 +0200

    redesign toggle medias

--- js/event.js
+++ js/event.js
@@ -31,48 +31,36 @@ function event() {
     document.querySelector('.header-block-medias-illustrations').addEventListener('click', () => {
        if(document.querySelector('.body-block-medias-illustrations').style.display === "block") {
            document.querySelector('.body-block-medias-illustrations').style.display = "none";
-           document.querySelector('.open-toggle-illustrations-1').style.display = "none";
-           document.querySelector('.open-toggle-illustrations-2').style.display = "none";
-           document.querySelector('.close-toggle-illustrations-1').style.display = "flex";
-           document.querySelector('.close-toggle-illustrations-2').style.display = "flex";
+           document.querySelector('.open-toggle-illustrations').style.display = "none";
+           document.querySelector('.close-toggle-illustrations').style.display = "flex";
        } else {
            document.querySelector('.body-block-medias-illustrations').style.display = "block";
-           document.querySelector('.open-toggle-illustrations-1').style.display = "flex";
-           document.querySelector('.open-toggle-illustrations-2').style.display = "flex";
-           document.querySelector('.close-toggle-illustrations-1').style.display = "none";
-           document.querySelector('.close-toggle-illustrations-2').style.display = "none";
+           document.querySelector('.open-toggle-illustrations').style.display = "flex";
+           document.querySelector('.close-toggle-illustrations').style.display = "none";
        }
     });
-
+git
     document.querySelector('.header-block-medias-pictures').addEventListener('click', () => {
         if(document.querySelector('.body-block-medias-pictures').style.display === "block") {
             document.querySelector('.body-block-medias-pictures').style.display = "none";
-            document.querySelector('.open-toggle-pictures-1').style.display = "none";
-            document.querySelector('.open-toggle-pictures-2').style.display = "none";
-            document.querySelector('.close-toggle-pictures-1').style.display = "flex";
-            document.querySelector('.close-toggle-pictures-2').style.display = "flex";
+            document.querySelector('.open-toggle-pictures').style.display = "none";
+            document.querySelector('.close-toggle-pictures').style.display = "flex";
         } else {
             document.querySelector('.body-block-medias-pictures').style.display = "block";
-            document.querySelector('.open-toggle-pictures-1').style.display = "flex";
-            document.querySelector('.open-toggle-pictures-2').style.display = "flex";
-            document.querySelector('.close-toggle-pictures-1').style.display = "none";
-            document.querySelector('.close-toggle-pictures-2').style.display = "none";
+            document.querySelector('.open-toggle-pictures').style.display = "flex";
+            document.querySelector('.close-toggle-pictures').style.display = "none";
         }
     });
 
     document.querySelector('.header-block-medias-videos').addEventListener('click', () => {
         if(document.querySelector('.body-block-medias-videos').style.display === "block") {
             document.querySelector('.body-block-medias-videos').style.display = "none";
-            document.querySelector('.open-toggle-videos-1').style.display = "none";
-            document.querySelector('.open-toggle-videos-2').style.display = "none";
-            document.querySelector('.close-toggle-videos-1').style.display = "flex";
-            document.querySelector('.close-toggle-videos-2').style.display = "flex";
+            document.querySelector('.open-toggle-videos').style.display = "none";
+            document.querySelector('.close-toggle-videos').style.display = "flex";
         } else {
             document.querySelector('.body-block-medias-videos').style.display = "block";
-            document.querySelector('.open-toggle-videos-1').style.display = "flex";
-            document.querySelector('.open-toggle-videos-2').style.display = "flex";
-            document.querySelector('.close-toggle-videos-1').style.display = "none";
-            document.querySelector('.close-toggle-videos-2').style.display = "none";
+            document.querySelector('.open-toggle-videos').style.display = "flex";
+            document.querySelector('.close-toggle-videos').style.display = "none";
         }
     });
 }
