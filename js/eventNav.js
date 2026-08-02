ffc75c6e33e5a1af218ac186e3af371fbcc36878
bodhi <ch.brice30@gmail.com>
2026-08-02 15:24:02 +0200

    create burger-menu-close-area

--- js/eventNav.js
+++ js/eventNav.js
@@ -3,6 +3,7 @@ function eventNav() {
         () => {
             document.querySelector('.burger-menu').style.display = "none";
             document.querySelector('.burger-menu-close').style.display = "flex";
+            document.querySelector('.burger-menu-close-area').style.display = "block";
             document.querySelector('nav').classList.add("mobile-nav");
             document.querySelector('nav ul').classList.add("mobile-nav-list");
             document.querySelector('body').style.overflow = "hidden";
@@ -11,12 +12,16 @@ function eventNav() {
     );
 
     document.querySelector(".burger-menu-close").addEventListener('click', closeNav)
-
+    document.querySelector(".burger-menu-close-area").addEventListener('click', closeNav)
     document.querySelector("main").addEventListener('click', closeNav)
 
     function closeNav() {
+
+        if (document.querySelector('nav').className !== 'mobile-nav') return;
+
         document.querySelector('.burger-menu').style.display = "flex";
         document.querySelector('.burger-menu-close').style.display = "none";
+        document.querySelector('.burger-menu-close-area').style.display = "none";
         document.querySelector('nav').classList.remove("mobile-nav");
         document.querySelector('nav ul').classList.remove("mobile-nav-list");
         document.querySelector('body').style.overflow = "visible";
