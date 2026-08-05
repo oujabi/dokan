8c71a1aa17f5d51245c606f969f228033a2587f4
bodhi <ch.brice30@gmail.com>
2026-08-05 12:55:12 +0200

    clean-code#4

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 'use strict';
 import {mobileNav} from "./js/mobile-nav.js";
-import {eventToggleMedias} from "./js/eventToggleMedias.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     mobileNav();
-    eventToggleMedias();
 });
 
 
